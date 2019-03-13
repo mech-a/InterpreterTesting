8df2100b04473c3a543e703ee373e2f67d6f885d
v3suv1us <V3SUV1US@users.noreply.github.com>
2019-03-13 15:40:35 -0700

    .gitignore purge

--- src/main/java/ReflectionTesting/CompileJSONToBOJO.java
+++ src/main/java/ReflectionTesting/CompileJSONToBOJO.java
@@ -4,21 +4,28 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.apache.commons.io.FileUtils;
 
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 
+/**
+ * JSON to BOJO converter
+ * Utilizes {@link FileUtils} and {@link File} to get a String from a file, in this case, a JSON file.
+ * {@link Gson} deserialization is then performed to transform the JSON into a BOJO of type {@link ExampleAuton}.
+ */
+
+
 public class CompileJSONToBOJO {
     public static void main(String[] args) throws IOException {
+        //Turn file into string
         String file = FileUtils.readFileToString(new File("exampleAuton.json"), Charset.defaultCharset());
-        Gson gson = new Gson();
 
+        //Create ExampleAuton object
+        Gson gson = new Gson();
         ExampleAuton autonLines = gson.fromJson(file, ExampleAuton.class);
 
-        for(MethodData method : autonLines.methods) {
+        for(MethodData method : autonLines.methods)
             System.out.println(method.toString());
-        }
-
-
     }
 }
